8966b7230d3ac62ea1cd9fddc9089f4b52947476
ravivarmak <ravivarmak3618@gmail.com>
2024-11-20 15:54:22 -0500

    Added a buffer to let user know the data is loading when directed to items page.

--- src/Items.tsx
+++ src/Items.tsx
@@ -9,6 +9,7 @@ import {
   Chip,
   Tooltip,
   Button,
+  Spinner,
 } from "@nextui-org/react";
 import { EditIcon } from "./components/icons/EditIcon";
 import { DeleteIcon } from "./components/icons/DeleteIcon";
@@ -38,18 +39,22 @@ const columns = [
 
 const Items: React.FC = () => {
   const [items, setItems] = useState<Item[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
   const [filterType, setFilterType] = useState<string>("All");
   const [statusFilter, setStatusFilter] = useState<string>("Any");
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchItems = async () => {
+      setLoading(true);
       try {
         const response = await fetch("http://localhost:5001/items");
         const data = await response.json();
         setItems(data);
       } catch (error) {
         console.error("Error fetching items:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -64,23 +69,26 @@ const Items: React.FC = () => {
     setStatusFilter(status);
   };
 
-  const filteredItems = items.filter(item => {
+  const filteredItems = items.filter((item) => {
     const matchesType = filterType === "All" || item.ITEMTYPE === filterType;
-    const matchesStatus = 
+    const matchesStatus =
       statusFilter === "Any" ||
       (statusFilter === "Available" && item.STATUS === "AVAILABLE") ||
       (statusFilter === "Checked In" && item.STATUS === "CHECKED IN") ||
       (statusFilter === "Checked Out" && item.STATUS === "CHECKED OUT");
-    
+
     return matchesType && matchesStatus;
   });
 
   // Function to make item available
   const handleMakeAvailable = async (itemID: number) => {
     try {
-      const response = await fetch(`http://localhost:5001/items/${itemID}/make-available`, {
-        method: "PUT",
-      });
+      const response = await fetch(
+        `http://localhost:5001/items/${itemID}/make-available`,
+        {
+          method: "PUT",
+        }
+      );
       if (response.ok) {
         alert("Item made available.");
         // Update item status locally or refetch items
@@ -103,11 +111,15 @@ const Items: React.FC = () => {
 
     switch (columnKey) {
       case "STATUS":
-        const chipColor = item.STATUS === "AVAILABLE" ? "success" :
-                          item.STATUS === "CHECKED OUT" ? "danger" : "warning";
+        const chipColor =
+          item.STATUS === "AVAILABLE"
+            ? "success"
+            : item.STATUS === "CHECKED OUT"
+            ? "danger"
+            : "warning";
         return (
-          <Chip 
-            className="capitalize" 
+          <Chip
+            className="capitalize"
             color={chipColor}
             size="sm"
             variant="flat"
@@ -176,60 +188,68 @@ const Items: React.FC = () => {
     navigate("/add-items");
   };
 
-  return (
-
-    
-    <div className="p-6">
-    {/* Header Section with Title on the left and Button on the right */}
-    <div className="flex justify-between items-center mb-6">
-      <h1 className="text-2xl font-bold">Library Items</h1>
-      <div className="ml-auto"> {/* This div will ensure alignment to the far right */}
-        <button onClick={handleBackToHome} className="back-to-home-button">
-          Back to Home Page
-        </button>
-        <button
-        onClick={handleAddItems}
-        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-      >
-        Add Items
-      </button>
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Spinner size="lg" />
       </div>
-    </div>
+    );
+  }
+
+  return (
+    <div className="p-6">
+      {/* Header Section with Title on the left and Button on the right */}
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-2xl font-bold">Library Items</h1>
+        <div className="ml-auto">
+          {" "}
+          {/* This div will ensure alignment to the far right */}
+          <button onClick={handleBackToHome} className="back-to-home-button">
+            Back to Home Page
+          </button>
+          <button
+            onClick={handleAddItems}
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            Add Items
+          </button>
+        </div>
+      </div>
 
       {/* Filter Section */}
       <div className="flex gap-4 mb-6">
         <label>
-          <input 
-            type="radio" 
-            value="All" 
-            checked={filterType === "All"} 
-            onChange={() => handleFilterChange("All")} 
+          <input
+            type="radio"
+            value="All"
+            checked={filterType === "All"}
+            onChange={() => handleFilterChange("All")}
           />
           All
         </label>
         <label>
-          <input 
-            type="radio" 
-            value="Book" 
-            checked={filterType === "Book"} 
-            onChange={() => handleFilterChange("Book")} 
+          <input
+            type="radio"
+            value="Book"
+            checked={filterType === "Book"}
+            onChange={() => handleFilterChange("Book")}
           />
           Book
         </label>
         <label>
-          <input 
-            type="radio" 
-            value="Movie" 
-            checked={filterType === "Movie"} 
-            onChange={() => handleFilterChange("Movie")} 
+          <input
+            type="radio"
+            value="Movie"
+            checked={filterType === "Movie"}
+            onChange={() => handleFilterChange("Movie")}
           />
           Movie
         </label>
         <label>
-          <input 
-            type="radio" 
-            value="Game" 
-            checked={filterType === "Game"} 
+          <input
+            type="radio"
+            value="Game"
+            checked={filterType === "Game"}
             onChange={() => handleFilterChange("Game")}
           />
           Game
@@ -239,38 +259,38 @@ const Items: React.FC = () => {
       {/* Status Filter Section */}
       <div className="flex gap-4 mb-6">
         <label>
-          <input 
-            type="radio" 
-            value="Any" 
-            checked={statusFilter === "Any"} 
-            onChange={() => handleStatusFilterChange("Any")} 
+          <input
+            type="radio"
+            value="Any"
+            checked={statusFilter === "Any"}
+            onChange={() => handleStatusFilterChange("Any")}
           />
           Any
         </label>
         <label>
-          <input 
-            type="radio" 
-            value="Available" 
-            checked={statusFilter === "Available"} 
-            onChange={() => handleStatusFilterChange("Available")} 
+          <input
+            type="radio"
+            value="Available"
+            checked={statusFilter === "Available"}
+            onChange={() => handleStatusFilterChange("Available")}
           />
           Available
         </label>
         <label>
-          <input 
-            type="radio" 
-            value="Checked In" 
-            checked={statusFilter === "Checked In"} 
-            onChange={() => handleStatusFilterChange("Checked In")} 
+          <input
+            type="radio"
+            value="Checked In"
+            checked={statusFilter === "Checked In"}
+            onChange={() => handleStatusFilterChange("Checked In")}
           />
           Checked In
         </label>
         <label>
-          <input 
-            type="radio" 
-            value="Checked Out" 
-            checked={statusFilter === "Checked Out"} 
-            onChange={() => handleStatusFilterChange("Checked Out")} 
+          <input
+            type="radio"
+            value="Checked Out"
+            checked={statusFilter === "Checked Out"}
+            onChange={() => handleStatusFilterChange("Checked Out")}
           />
           Checked Out
         </label>
